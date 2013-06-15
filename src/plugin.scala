14939bcbcbdbd7f32ae5a51327a7d7559246b625
RainWarrior <rainwarrior@fry.su>
2013-06-15 20:25:35 +0400

    Plugin now works in both enviroments

--- src/plugin.scala
+++ src/plugin.scala
@@ -30,6 +30,7 @@ of this Program grant you additional permission to convey the resulting work.
 package rainwarrior.hooks
 
 import cpw.mods.fml.relauncher.{ IClassTransformer, IFMLCallHook, IFMLLoadingPlugin }
+import cpw.mods.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper.{ INSTANCE => mapper }
 import org.objectweb.asm
 import asm.{ ClassReader, ClassWriter, Opcodes }
 import asm.tree._
@@ -39,101 +40,119 @@ import collection.JavaConversions._
 import java.util.{ Map => JMap }
 import java.io.PrintWriter
 
-@IFMLLoadingPlugin.TransformerExclusions(value = Array("rainwarrior", "scala", "java", "cpw"))
-class Plugin extends IFMLLoadingPlugin with IFMLCallHook{
-//  type unit = typeOf[runtime.BoxedUnit]
-  override val getLibraryRequestClass = null
-  override val getASMTransformerClass = Array(classOf[Transformer].getName)
-  override val getModContainerClass: String = null
-  override val getSetupClass = classOf[Plugin].getName
+@IFMLLoadingPlugin.TransformerExclusions(value = Array("rainwarrior", "scala"))
+class Plugin extends IFMLLoadingPlugin with IFMLCallHook {
+  override def getLibraryRequestClass: Array[String] = null
+  override def getASMTransformerClass = Array("rainwarrior.hooks.Transformer")
+  override def getModContainerClass: String = null
+  override def getSetupClass = "rainwarrior.hooks.Plugin"
   override def injectData(data: JMap[String, AnyRef]) {}
-  override def call() = {
+  override def call(): Void = {
     println("Hello, World! From CoreMod!")
     null
   }
 }
 
 class Transformer extends IClassTransformer {
-  override def transform(name: String, transformedName: String, data: Array[Byte]) = {
-    val node = new ClassNode
-    val reader = new ClassReader(data)
-    reader.accept(node, 0)
-
-    name match {
-      case n  if (n == "net.minecraft.world.World" || n == "net.minecraft.world.ChunkCache") =>
-        println(s"transforming: $name");
-        node.methods = for (m <- node.methods) yield m.name match {
-          case "isBlockOpaqueCube" =>
-            println("FOUND isBlockOpaqueCube")
-            //for(inst <- m.instructions.toArray) println(inst)
-            val l1 = m.instructions.toArray.collectFirst{case i: JumpInsnNode if i.getOpcode == GOTO => i.label}.get
-            val pos = m.instructions.toArray.collectFirst{case i: FrameNode => i}.get
-            val list = new InsnList
-            list.add(new VarInsnNode(ALOAD, 0))
-            if(name == "net.minecraft.world.ChunkCache") {
-              list.add(new FieldInsnNode(
-                GETFIELD,
-                "net/minecraft/world/ChunkCache",
-                "worldObj",
-                "Lnet/minecraft/world/World;"))
-            }
-            list.add(new VarInsnNode(ILOAD, 1))
-            list.add(new VarInsnNode(ILOAD, 2))
-            list.add(new VarInsnNode(ILOAD, 3))
-            list.add(new MethodInsnNode(
-              INVOKESTATIC,
-              "rainwarrior/hooks/MovingRegistry",
-              "isMoving",
-              "(Lnet/minecraft/world/World;III)Z"))
-            val l2 = new LabelNode
-            list.add(new JumpInsnNode(IFEQ, l2))
-            list.add(new InsnNode(ICONST_0))
-            list.add(new JumpInsnNode(GOTO, l1))
-            list.add(l2)
-            list.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null))
-            m.instructions.insert(pos, list)
-            m
-          case _ => m
-        }
-        val writer = new ClassWriter(ClassWriter.COMPUTE_MAXS)
-        node.accept(writer)
-        //val checker = new TraceClassVisitor(writer, new ASMifier, new PrintWriter(System.out))
-        //node.accept(checker)
-        writer.toByteArray
-        //data
-      case "net.minecraft.client.renderer.RenderBlocks" =>
-        println(s"transforming: $name")
-        node.methods = for (m <- node.methods) yield m.name match {
-          case "renderBlockByRenderType" =>
-            println("FOUND renderBlockByRenderType")
-            for(inst <- m.instructions.toArray) println(inst)
-            val old = m.instructions.toArray.collectFirst{case i: MethodInsnNode => i}.get
-            val list = new InsnList
-            list.add(new VarInsnNode(ILOAD, 2))
-            list.add(new VarInsnNode(ILOAD, 3))
-            list.add(new VarInsnNode(ILOAD, 4))
-            list.add(new MethodInsnNode(
-              INVOKESTATIC,
-              "rainwarrior/hooks/MovingRegistry",
-              "getRenderType",
-              "(Lnet/minecraft/block/Block;III)I"))
-            m.instructions.insert(old, list)
-            m.instructions.remove(old)
-            println("transformed")
-            for(inst <- m.instructions.toArray) println(inst)
-            m
-          case _ => m
-        }
-        val writer = new ClassWriter(ClassWriter.COMPUTE_MAXS)
-        node.accept(writer)
-        //val checker = new TraceClassVisitor(writer, new ASMifier, new PrintWriter(System.out))
-        //node.accept(checker)
-        writer.toByteArray
-        //data
-      case "net.minecraft.client.renderer.TileEntityRenderer" =>
-        println(s"transforming: $name")
-        data
-      case _ => data
+  type MethodChecker = (String, MethodNode) => Boolean
+  type InsTransformer = MethodNode => Unit
+  lazy val deobfEnv = this.getClass.getClassLoader.getResource("net/minecraft/world/World.class") != null
+  def transformIsBlockOpaqueCube(isChunkCache: Boolean)(m: MethodNode) {
+    println(s"FOUND isBlockOpaqueCube")
+    val l1 = m.instructions.toArray.collectFirst{case i: JumpInsnNode if i.getOpcode == GOTO => i.label}.get
+    val pos = m.instructions.toArray.collectFirst{case i: FrameNode => i}.get
+    val list = new InsnList
+    list.add(new VarInsnNode(ALOAD, 0))
+    if(isChunkCache) {
+      list.add(new FieldInsnNode(
+        GETFIELD,
+        "net/minecraft/world/ChunkCache",
+        if(deobfEnv) "worldObj" else "field_72815_e",
+        "Lnet/minecraft/world/World;"))
     }
+    list.add(new VarInsnNode(ILOAD, 1))
+    list.add(new VarInsnNode(ILOAD, 2))
+    list.add(new VarInsnNode(ILOAD, 3))
+    list.add(new MethodInsnNode(
+      INVOKESTATIC,
+      "rainwarrior/hooks/MovingRegistry",
+      "isMoving",
+      "(Lnet/minecraft/world/World;III)Z"))
+    val l2 = new LabelNode
+    list.add(new JumpInsnNode(IFEQ, l2))
+    list.add(new InsnNode(ICONST_0))
+    list.add(new JumpInsnNode(GOTO, l1))
+    list.add(l2)
+    list.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null))
+    m.instructions.insert(pos, list)
+  }
+  def transformRenderBlockByRenderType(m: MethodNode) {
+    println(s"FOUND renderBlockByRenderType")
+    //for(inst <- m.instructions.toArray) println(inst)
+    val old = m.instructions.toArray.collectFirst{case i: MethodInsnNode => i}.get
+    val list = new InsnList
+    list.add(new VarInsnNode(ILOAD, 2))
+    list.add(new VarInsnNode(ILOAD, 3))
+    list.add(new VarInsnNode(ILOAD, 4))
+    list.add(new MethodInsnNode(
+      INVOKESTATIC,
+      "rainwarrior/hooks/MovingRegistry",
+      "getRenderType",
+      "(Lnet/minecraft/block/Block;III)I"))
+    m.instructions.insert(old, list)
+    m.instructions.remove(old)
+  }
+  val classData = Map[String, Tuple3[MethodChecker, MethodChecker, InsTransformer]](
+    "net.minecraft.world.World" -> ((
+      (_, m) => m.name == "isBlockOpaqueCube",
+      (n, m) => mapper.mapMethodName(n, m.name, "(III)Z") == "func_72804_r",
+      transformIsBlockOpaqueCube(false)
+    )),
+    "net.minecraft.world.ChunkCache" -> ((
+      (_, m) => m.name == "isBlockOpaqueCube",
+      (n, m) => mapper.mapMethodName(n, m.name, "(III)Z") == "func_72804_r",
+      transformIsBlockOpaqueCube(true)
+    )),
+    "net.minecraft.client.renderer.RenderBlocks" -> ((
+      (_, m) => m.name == "renderBlockByRenderType",
+      (n, m) => mapper.mapMethodName(n, m.name, "(Lapa;III)Z") == "func_78612_b" && m.desc == "(Lapa;III)Z",
+      transformRenderBlockByRenderType
+    )),
+    "net.minecraft.client.renderer.tileentity.TileEntityRenderer" -> ((
+      (_, m) => m.name == "<init>",
+      (_, m) => m.name == "<init>",
+      { m: MethodNode =>
+        println("FOUND <init>")
+        m.access &= ~ACC_PRIVATE
+        m.access &= ~ACC_PROTECTED
+        m.access |= ACC_PUBLIC
+      }
+    )))
+  override def transform(name: String, tName: String, data: Array[Byte]) = {
+    //println(s"checking: $name, $tName")
+    //val getMethodMap = mapper.getClass.getDeclaredMethod("getMethodMap", classOf[String])
+    //getMethodMap.setAccessible(true)
+    //val classNameBiMap = mapper.getClass.getDeclaredField("classNameBiMap")
+    //classNameBiMap.setAccessible(true)
+    if(classData.keys.contains(tName)) {
+      println(s"transforming: $tName")
+      val (ch1, ch2, tr) = classData(tName)
+      val node = new ClassNode
+      val reader = new ClassReader(data)
+      reader.accept(node, 0)
+
+      for(m <- node.methods) {
+        if((deobfEnv && ch1(name, m)) || (!deobfEnv && ch2(name, m))) {
+          tr(m)
+        }
+      }
+
+      val writer = new ClassWriter(ClassWriter.COMPUTE_MAXS)
+      node.accept(writer)
+      //val checker = new TraceClassVisitor(writer, new ASMifier, new PrintWriter(System.out))
+      //node.accept(checker)
+      writer.toByteArray
+      //data
+    } else data
   }
 }
