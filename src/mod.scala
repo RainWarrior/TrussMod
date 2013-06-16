4980bf4b9209b29074d615687795efde432b78a5
RainWarrior <rainwarrior@fry.su>
2013-06-17 01:56:53 +0400

    Added mcmod.info, changed verison to alpha

--- src/mod.scala
+++ src/mod.scala
@@ -114,7 +114,7 @@ class ClientProxy extends CommonProxy {
   modLanguage = "scala",
   modid = modId,
   name = modName,
-  version = "0.01",
+  version = "alpha",
   dependencies = "required-after:Forge@[7.8.0.701,);required-after:FML@[5.2.10,)"
 )
 @NetworkMod(
