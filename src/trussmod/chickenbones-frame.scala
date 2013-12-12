2db34536f5a48f2ad390e760c0f1c4c9cf4f350d
RainWarrior <rainwarrior@fry.su>
2013-12-12 18:06:51 +0400

    Fixed FMP frame mining speed

--- src/trussmod/chickenbones-frame.scala
+++ src/trussmod/chickenbones-frame.scala
@@ -65,6 +65,8 @@ import codechicken.microblock.CommonMicroblock
 class ChickenBonesFramePart(val id: Int) extends TMultiPart with Frame with JPartialOcclusion with TNormalOcclusion {
   override val getType = "Frame"
 
+  override def getStrength(mop: MovingObjectPosition, player: EntityPlayer) = 5F
+
   override def getDrops: JIterable[ItemStack] = Seq(new ItemStack(id, 1, 0))
   //override val getSubParts: JIterable[IndexedCuboid6] = Seq(new IndexedCuboid6(0, new Cuboid6(-eps, -eps, -eps, 1 + eps, 1 + eps, 1 + eps)))
 
