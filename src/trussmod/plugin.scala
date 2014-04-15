9993426321b5eea24b2938f2cd537ed82bd8565d
RainWarrior <rainwarrior@fry.su>
2014-04-15 18:32:09 +0400

    updated srg names in coremod, isBlockOpaqueCube is gone :(

--- src/trussmod/plugin.scala
+++ src/trussmod/plugin.scala
@@ -64,37 +64,7 @@ class Transformer extends IClassTransformer {
   lazy val deobfEnv = this.getClass.getClassLoader.getResource("net/minecraft/world/World.class") != null
   lazy val blockClass = mapper.unmap("net/minecraft/block/Block")
   lazy val teClass = mapper.unmap("net/minecraft/tileentity/TileEntity")
-  def transformIsBlockOpaqueCube(isChunkCache: Boolean)(m: MethodNode) {
-    log.finer(s"TrussMod: FOUND isBlockOpaqueCube")
-    val l1 = m.instructions.toArray.collectFirst{case i: JumpInsnNode if i.getOpcode == GOTO => i.label}.get
-    val pos = m.instructions.toArray.collectFirst{case i: FrameNode => i}.get
-    val list = new InsnList
-    list.add(new VarInsnNode(ALOAD, 0))
-    if(isChunkCache) {
-      list.add(new FieldInsnNode(
-        GETFIELD,
-        "net/minecraft/world/ChunkCache",
-        if(deobfEnv) "worldObj" else "field_72815_e",
-        "Lnet/minecraft/world/World;"))
-    }
-    list.add(new VarInsnNode(ILOAD, 1))
-    list.add(new VarInsnNode(ILOAD, 2))
-    list.add(new VarInsnNode(ILOAD, 3))
-    list.add(new MethodInsnNode(
-      INVOKESTATIC,
-      "rainwarrior/hooks/MovingRegistry",
-      "isMoving",
-      "(Lnet/minecraft/world/World;III)Z"))
-    val l2 = new LabelNode
-    list.add(new JumpInsnNode(IFEQ, l2))
-    list.add(new InsnNode(ICONST_0))
-    list.add(new JumpInsnNode(GOTO, l1))
-    list.add(l2)
-    list.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null))
-    m.instructions.insert(pos, list)
-  }
   def transformRenderBlockByRenderType(m: MethodNode) {
-    log.finer(s"TrussMod: FOUND renderBlockByRenderType")
     //for(inst <- m.instructions.toArray) log.finer(inst)
     val old = m.instructions.toArray.collectFirst{case i: MethodInsnNode => i}.get
     val list = new InsnList
@@ -110,7 +80,6 @@ class Transformer extends IClassTransformer {
     m.instructions.remove(old)
   }
   def transformRenderTileEntity(m: MethodNode) {
-    log.finer(s"TrussMod: FOUND renderTileEntity")
     val old = m.instructions.toArray.collect { case i: MethodInsnNode => i }.last
     m.instructions.insert(old, new InsnNode(POP))
     m.instructions.insert(old, new MethodInsnNode(
@@ -122,29 +91,19 @@ class Transformer extends IClassTransformer {
     m.instructions.remove(old)
   }
   val classData = Map[String, Tuple3[MethodChecker, MethodChecker, InsTransformer]](
-    "net.minecraft.world.World" -> ((
-      (_, m) => m.name == "isBlockOpaqueCube",
-      (n, m) => mapper.mapMethodName(n, m.name, "(III)Z") == "func_72804_r",
-      transformIsBlockOpaqueCube(false)
-    )),
-    "net.minecraft.world.ChunkCache" -> ((
-      (_, m) => m.name == "isBlockOpaqueCube",
-      (n, m) => mapper.mapMethodName(n, m.name, "(III)Z") == "func_72804_r",
-      transformIsBlockOpaqueCube(true)
-    )),
     "net.minecraft.client.renderer.RenderBlocks" -> ((
       (_, m) => m.name == "renderBlockByRenderType",
-      (n, m) => mapper.mapMethodName(n, m.name, s"(L$blockClass;III)Z") == "func_78612_b" && m.desc == s"(L$blockClass;III)Z",
+      (n, m) => mapper.mapMethodName(n, m.name, m.desc) == "func_147805_b",
       transformRenderBlockByRenderType
     )),
     "net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher" -> ((
       (_, m) => m.name == "renderTileEntity",
-      (n, m) => mapper.mapMethodName(n, m.name, s"(L$teClass;F)V") == "func_147544_a",
+      (n, m) => mapper.mapMethodName(n, m.name, m.desc) == "func_147544_a",
       transformRenderTileEntity
     )))
   override def transform(name: String, tName: String, data: Array[Byte]) = {
     if(classData.keys.contains(tName)) { // patch table transformer
-      println(s"TrussMod: transforming: $tName")
+      log.finer(s"TrussMod: transforming: $tName")
       val (ch1, ch2, tr) = classData(tName)
       val node = new ClassNode
       val reader = new ClassReader(data)
@@ -152,6 +111,7 @@ class Transformer extends IClassTransformer {
 
       for(m@(_m: MethodNode) <- node.methods) {
         if((deobfEnv && ch1(name, m)) || (!deobfEnv && ch2(name, m))) {
+          log.finer(s"TrussMod: FOUND ${m.name} ${m.desc}")
           tr(m)
         }
       }
